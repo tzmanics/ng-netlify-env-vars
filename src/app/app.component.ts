96921d9f3da0b4ec225fd7383aa02e8da83e69a0
tzmanics <tzmanics@gmail.com>
2022-01-11 21:45:47 -0500

    adds setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { getEnvVarList } from '../../utils/envVarList';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'env-var-example';
+  processEnv: any = getEnvVarList();
 }
